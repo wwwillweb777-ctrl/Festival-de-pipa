4789dabed0f5ab41796f427c9f836ccc0b43d4ea
wwwillweb777-ctrl <wwwill.web777@gmail.com>
2026-08-10 23:39:49 -0300

    Update Firebase configuration and visitor ID logic

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,24 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyATcfKpiac2DKdSjafq-NwAnuMBnxD9VBE",
-    authDomain: "truco-mineiro-1df23.firebaseapp.com",
-    databaseURL: "https://truco-mineiro-1df23-default-rtdb.firebaseio.com",
-    projectId: "truco-mineiro-1df23",
-    storageBucket: "truco-mineiro-1df23.firebasestorage.app",
-    messagingSenderId: "597079073519",
-    appId: "1:597079073519:web:b1c8d7e6f5a4b3c9d2e1f3"
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-function dataAtual() {
-    return new Date().toLocaleString('pt-BR');
-}
 const SENHA_ADMIN = "WJ321";
 const NOME_REMETENTE_ADMIN = "Programador";
 let acessoLiberado = false;
+let nomeUsuarioChat = "";
+
 const ID_VISITANTE = localStorage.getItem("idVisitante") || ("usr_" + Date.now() + "_" + Math.random().toString(36).substr(2, 9));
 localStorage.setItem("idVisitante", ID_VISITANTE);
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqsOq01FhDq9z9Wn0tK9XbP8mIeQ9z7yM",
+    authDomain: "willobras.firebaseapp.com",
+    databaseURL: "https://willobras-default-rtdb.firebaseio.com",
+    projectId: "willobras",
+    storageBucket: "willobras.firebasestorage.app",
+    messagingSenderId: "66370500400",
+    appId: "1:66370500400:web:42d6569ca2c8172ddf3af5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+function dataAtual() {
+    return new Date().toLocaleString('pt-BR');
+}
